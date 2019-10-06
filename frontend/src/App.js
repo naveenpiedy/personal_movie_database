19e3d94a1d28b632625a6314083e8e626c916a1f
Naveen Piedy <nav.bugs.thepoet@gmail.com>
2019-10-06 12:13:03 -0400

    Added the react files necessary for navbar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,50 @@
 import React from 'react';
 import logo from './logo.svg';
+import 'bootstrap/dist/css/bootstrap.css';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import { CustomNavbar } from './customnavbar.js';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.movieSearch = this.movieSearch.bind(this);
+    this.localSearch = this.localSearch.bind(this);
+    this.state = {
+      searchState: "home",
+    }
+  }
+  movieSearch() {
+    this.setState({
+      searchState: "search_internet"
+    })
+  }
+
+  localSearch() {
+    this.setState({
+      searchState: "search_local"
+    })
+  }
+
+  render() {
+    var tag_to_call = null;
+    switch (this.state.searchState) {
+      case "home":
+        tag_to_call = <p>Home</p>;
+        break;
+      case "search_internet":
+        tag_to_call = <p>Searching Internet</p>
+        break;
+      case "search_local":
+        tag_to_call = <p>Searching Local</p>
+        break;
+    }
+
+    return (<div>
+      <CustomNavbar movieSearch={this.movieSearch} localSearch={this.localSearch} />
+      {tag_to_call}
+    </div>);
+  }
 }
 
 export default App;
