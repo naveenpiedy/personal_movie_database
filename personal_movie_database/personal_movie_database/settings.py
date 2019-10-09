c06ba06ab33718599135976edb7e255e2494ce8f
Naveen Piedy <nav.bugs.thepoet@gmail.com>
2019-10-08 20:43:43 -0400

    Added DRF enpoints to use TMDB api

--- personal_movie_database/personal_movie_database/settings.py
+++ personal_movie_database/personal_movie_database/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'movies_fetch',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
@@ -123,3 +124,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+REST_FRAMEWORK = {
+    # Use Django's standard `django.contrib.auth` permissions,
+    # or allow read-only access for unauthenticated users.
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10
+}
