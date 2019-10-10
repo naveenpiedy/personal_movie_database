725d5fc3141ff224e74b0e66f334046dbc5ac91c
Naveen Piedy <nav.bugs.thepoet@gmail.com>
2019-10-10 19:39:21 -0400

    Added card and way to list searched movies on the UI

--- frontend/src/searchInternet.js
+++ frontend/src/searchInternet.js
@@ -4,6 +4,10 @@ import { Container, Row, Col } from 'reactstrap';
 import { Button, ButtonGroup } from 'reactstrap';
 import styled from 'styled-components/macro';
 import axios from 'axios';
+import {
+    Card, CardImg, CardText, CardBody,
+    CardTitle, CardSubtitle
+} from 'reactstrap';
 
 const Search_Buttons = styled(Button)`
 margin: 10px;
@@ -23,28 +27,60 @@ box-shadow: 0 10px 15px -3px rgba(135,206,250, 0.3), 0 4px 6px -2px rgba(255, 25
 `;
 
 export class SearchInternet extends React.Component {
+
     constructor(props) {
         super(props);
         this.getmovie = this.getmovie.bind(this);
-        this.state = {value: '', result:''};
+        this.state = { value: '', result: '', expansion: 'Search' };
         this.handleChange = this.handleChange.bind(this);
     }
 
     handleChange(event) {
-        this.setState({value: event.target.value});
-      }
+        this.setState({ value: event.target.value });
+    }
 
     getmovie() {
-        axios.get("/movies/?movie_name=\""+this.state.value+"\"&format=json").then(response => {
-            this.setState({result: response.data});
-          })
-          .catch(function (error) {
-            console.log(error);
-          });
-        console.log(this.state.result);
-      }
-    
+        axios.get("/movies/?movie_name=\"" + this.state.value + "\"&format=json").then(response => {
+            this.setState({ result: response.data['results'], expansion: 'Search' });
+        })
+            .catch(function (error) {
+                console.log(error);
+            });
+        console.log(this.state.result)
+    }
+
     render() {
+        if (this.state.result != '' && this.state.expansion == 'Search') {
+            var list_items = this.state.result.map((item) => {
+                return (
+                    <div class="mt-3 mb-3">
+                        <Card>
+                            <CardBody>
+                                <Row>
+                                    <Col md={{ size: 4 }}>
+                                        <img src={"https://image.tmdb.org/t/p/w780/" + item["poster_path"]} class="img-thumbnail img-fluid" />
+                                    </Col>
+                                    <Col md={{ size: 8 }}>
+                                        <CardTitle className="display-4">{item["title"]}</CardTitle>
+                                        <br />
+                                        <CardText className="h5">Overview: {item["overview"]}</CardText>
+                                        <br />
+                                        <CardText>Release Date: {item["release_date"]}</CardText>
+                                        <br />
+                                        <CardText>Vote Average: {item["vote_average"]}</CardText>
+                                        <br />
+                                        <CardText>Vote Count: {item["vote_count"]}</CardText>
+                                    </Col>
+                                </Row>
+
+                            </CardBody>
+                        </Card>
+                    </div>
+                )
+            })
+        } else {
+            var list_items = <div></div>
+        }
         return (
             <Container fluid className="mt-5">
                 <Row><Col md={{ size: 10, offset: 1 }}>
@@ -53,14 +89,15 @@ export class SearchInternet extends React.Component {
                             <Styled_Input id="keyword" placeholder="Enter keyword" type="text" value={this.state.value} onChange={this.handleChange} />
                         </Styled_IG>
                         <div className="text-center mt-4">
-                                <Search_Buttons id="search_other" onClick={this.getmovie}>Search Other</Search_Buttons>
-                                <Search_Buttons id="search_movies">Search Movie</Search_Buttons>
-                                <Search_Buttons id="search_people">Search People</Search_Buttons>
+                            <Search_Buttons id="search_other" onClick={this.getmovie}>Search Other</Search_Buttons>
+                            <Search_Buttons id="search_movies">Search Movie</Search_Buttons>
+                            <Search_Buttons id="search_people">Search People</Search_Buttons>
                         </div>
                     </div>
                 </Col>
                 </Row>
-                <Row><Col md={{ size: 10, offset: 1 }}>
+                <Row><Col md={{ size: 6, offset: 3 }}>
+                    {list_items}
                 </Col>
                 </Row>
             </Container>);
