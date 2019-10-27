eb1f1be56f5bca82ca6b0d184149d8576d786226
Naveen Piedy <nav.bugs.thepoet@gmail.com>
2019-10-26 21:36:24 -0400

    Expanded on moviedetails
    - Increased the modal width
    - Getting movie details
    - Refactored the code

--- frontend/src/moviedetails.js
+++ frontend/src/moviedetails.js
@@ -1,6 +1,13 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
+import styled from 'styled-components/macro';
+import axios from 'axios';
+
+const Styled_Modal = styled(Modal)`
+width: 90vw;
+max-width: 90vw;
+`;
+
 
 export class MovieDetails extends React.Component {
     constructor(props) {
@@ -14,16 +21,18 @@ export class MovieDetails extends React.Component {
         if (!this.props.show) {
             return null
         }
+        if (this.props.showMovie!=-1){
+            console.log(this.props.movie);
+        }
         return (
-            <Modal isOpen={this.props.show}>
-                <ModalHeader toggle={(e) => { this.onClose(e) }}>{this.props.movie}</ModalHeader>
+            <Styled_Modal isOpen={this.props.show}>
+                <ModalHeader toggle={(e) => { this.onClose(e) }}>{this.props.movie["tmdb_result"]["original_title"]}</ModalHeader>
                 <ModalBody>
-                    Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
                 </ModalBody>
                 <ModalFooter>
                     <Button color="secondary" id="-1" onClick={(e) => { this.onClose(e) }}>Cancel</Button>
                 </ModalFooter>
-            </Modal>
+            </Styled_Modal>
         )
     }
 }
